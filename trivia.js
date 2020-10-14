9756b6541a287abfd1bbd78fed27a959aab24e32
neto-es <ernesto867es@gmail.com>
2020-10-14 03:09:43 -0500

    arreglo

--- trivia.js
+++ trivia.js
@@ -1,33 +1,3 @@
-// const response = {
-//     response_code: 0,
-//     results: [
-//         {},
-//         {}
-//     ]
-// }
-
-// let datos = {};
-
-function getQuestions() {
-    const questionsQuantity = document.getElementById('questions-number').value
-    const questionsCategory = document.getElementById('question-category').value
-    const questionsDifficulty = document.getElementById('questions-difficulty').value
-    const questionsType = document.getElementById('questions-type').value
-    console.log(questionsQuantity)
-    console.log(questionsCategory)
-    console.log(questionsDifficulty)
-    console.log(questionsType)
-    fetch(`https://opentdb.com/api.php?amount=${questionsQuantity}&category=${questionsCategory}&difficulty=${questionsDifficulty}&type=${questionsType}`)
-    //fetch(`https://opentdb.com/api.php?amount=${questionsQuantity}&difficulty=${questionsDifficulty}`)
-    //https://opentdb.com/api.php?amount=10&difficulty=easy&type=multiple
-    .then(response => response.json())
-        .then(data => printCards(data.results))
-}
-
-// console.log(datos);
-
-
-// esta función obtiene las categorías
 function getCategories() {
     const endpointCategories = 'https://opentdb.com/api_category.php'
     fetch(endpointCategories)
@@ -40,67 +10,156 @@ function getCategories() {
           
             console.error(error)
         })
-}
-// Aquí se muestran las categorías
-function printCategories(categories) {
-    const selectCategories = document.getElementById('question-category')
+  }
+  function printCategories(categories) {
+    const selectCategories = document.getElementById('select-categories')
+  
     let html = ''
     categories.forEach(category => {
         html += `<option value="${category.id}"> ${category.name}</option>`
     });
-
     selectCategories.innerHTML = html
-}
-
-getCategories()
-
-function selectCategory() {
-    const categoryID = document.getElementById('question-category').value
+  }
+  
+  getCategories()
+  
+  
+  function selectCategory() {
+    const categoryID = document.getElementById('select-categories').value
     console.log(categoryID)
     return categoryID
-}
-
-function printCards(questions) {
-    const container = document.getElementById('container-cards');
-    container.innerHTML = '';
-    questions.forEach(question => {
-        const card = returnCardHTML(question);
-        container.innerHTML += card;
-    });
-    // poner las preguntas en mi página web
-}
-
-function returnCardHTML(q) {
-    const card = `<div class="card">
-                    <div class="card-body">
-                    <h5 class="card-title">${q.category}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${q.question}</h6>
-                        ${returnAnswersHTML(q.correct_answer, q.incorrect_answers)}           
-                    </div>
-                </div>`
-    return card;
-}
-
-
-function returnAnswersHTML(correct, incorrects) {
-    const correctHTML = `<div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            ${correct}
-                            </label>
-                        </div>`;
-
-
-    let incorrectHTML = '';
-    incorrects.forEach((incorrect) => {
-        incorrectHTML += `<div class="form-check">
-                            <input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" checked>
-                            <label class="form-check-label" for="exampleRadios1">
-                            ${incorrect}
-                            </label>
-                        </div>`;
+  }
+  
+  function selectNumber() {
+    const numberAmount = document.getElementById('selectNumberQuestions').value
+    console.log(numberAmount)
+    return numberAmount
+  }
+  
+  function selectType() {
+    const Typequestion = document.getElementById('selectTypeQuest').value
+    console.log(Typequestion)
+    return Typequestion
+  }
+  
+  function selectDifficult() {
+    const difficultQuestion = document.getElementById('selectDifficult').value
+    console.log(difficultQuestion)
+    return difficultQuestion
+  }
+  
+  function ReceiveData() {
+    const endpoint = `https://opentdb.com/api.php?amount=${selectNumber()}&category=${selectCategory()}&difficulty=${selectDifficult()}&type=${selectType()}`
+    console.log(endpoint)
+    return endpoint
+  }
+  
+  
+  function getDataTrivia() {
+    fetch(ReceiveData())
+        .then(response => response.json())
+        .then(dataJsonT => {
+            console.log(dataJsonT)
+            if(dataJsonT.response_code == 0) {
+              PrintQuestions(dataJsonT.results)
+            } else {
+              alert('No hay datos')
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })
+  }
+  function PrintQuestions(Questions) {
+    const PrintQuestions = document.getElementById(`Questions`)
+  
+  
+  if (selectType() =='multiple'){
+  
+     const CorrectQuest = Questions.map(function(CorrectAns) {
+        return CorrectAns.correct_answer
     })
-
-
-    return correctHTML + incorrectHTML;
-}
+  
+     const IncorrectQuest1 = Questions.map(function(incorrectAnswer) {
+         return incorrectAnswer.incorrect_answers[0]
+     })
+     const IncorrectQuest2 = Questions.map(function(incorrectAnswer) {
+        return incorrectAnswer.incorrect_answers[1]
+    })
+    const IncorrectQuest3 = Questions.map(function(incorrectAnswer) {
+        return incorrectAnswer.incorrect_answers[2]
+    })
+  
+  
+     let html = ``
+     Questions.forEach((question, index)=>{
+         html += `<div class="card m-2 card-boddy">
+                    <div class="card-body">
+                      ${question.question}
+                      <div class="input-group">
+                        <div class="input-group-prepend m-2">
+                          <div class="input-group-text">
+                            <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                            ${IncorrectQuest1[index]}
+                          </div>
+                        </div>
+                        <div class="input-group-prepend m-2">
+                          <div class="input-group-text">
+                            <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                            ${IncorrectQuest2[index]}
+                          </div>
+                        </div>
+                        <div class="input-group-prepend m-2">
+                          <div class="input-group-text">
+                            <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                            ${IncorrectQuest3[index]}
+                          </div>
+                        </div>
+                        <div class="input-group-prepend m-2">
+                          <div class="input-group-text">
+                             <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                             ${CorrectQuest[index]}
+                          </div>
+                        </div>
+                      </div>
+                     </div>
+                  </div>`
+     })
+     PrintQuestions.innerHTML = html
+  }
+  else if(selectType() == 'boolean'){
+    const CorrectQuest = Questions.map(function(CorrectAns) {
+       return CorrectAns.correct_answer
+   })
+  
+    const IncorrectQuest1 = Questions.map(function(incorrectAnswer) {
+        return incorrectAnswer.incorrect_answers[0]
+    })
+  
+  
+    let html = ``
+    Questions.forEach((question, index)=>{
+        html +=`<div class="card m-2 card-boddy">
+                  <div class="card-body">
+                    ${question.question}
+                    <div class="input-group">
+                      <div class="input-group-prepend m-2">
+                        <div class="input-group-text">
+                          <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                          ${IncorrectQuest1[index]}
+                        </div>
+                      </div>
+                      <div class="input-group-prepend m-2">
+                        <div class="input-group-text">
+                          <input type="radio" name="${index}"aria-label="Radio button for following text input">
+                          ${CorrectQuest[index]}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>`
+    })
+    PrintQuestions.innerHTML = html
+  } 
+  
+  }
